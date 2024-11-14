25c1f376dbd70e96e15b7174d08ac41eb553da35
brianandowen <brianandowen@gmail.com>
2024-11-14 16:15:45 +0800

    更新：資料庫連線

--- db.php
+++ db.php
@@ -11,11 +11,4 @@ $dbPassword = "";
 
 $conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbname);
 
-if (!$conn) {
-    die("連線失敗: " . mysqli_connect_error());
-} else {
-    echo "資料庫連線成功！";
-}
-
-
 ?>
