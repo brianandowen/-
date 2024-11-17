90b0c307a100e13eb59ffb9ef1ef03a9312ea82c
brianandowen <brianandowen@gmail.com>
2024-11-17 15:12:45 +0800

    新增：footer

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer style="position: fixed; bottom: 5%; width: 100%; text-align: center;">
+    <small>
+        Copyright © 2024 輔大資管學系 二甲 陳庭毅 412401317<br>
+        Copyright © 2024 輔大資管學系 二甲 吳宇燊 412261121
+    </small>
+</footer>
