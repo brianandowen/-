ac8088e552e327d4e5894f6a67379c35b2a6d29c
brianandowen <brianandowen@gmail.com>
2024-11-18 17:17:30 +0800

    更新：mt-5

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
-<footer style="background-color: #003366; color: white;" class="text-center py-3">
+
+<footer style="background-color: #003366; color: white;" class="text-center py-3 mt-5">
+    <div class="container">
     <small>
         Copyright © <?php echo date('Y'); ?> 輔大資管學系 二甲 陳庭毅 412401317<br>
         Copyright © <?php echo date('Y'); ?> 輔大資管學系 二甲 吳宇燊 412261121
     </small>
+    </div>
 </footer>
