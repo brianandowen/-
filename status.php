03d63206d3bc2c37f9751982dba214735c80e519
brianandowen <brianandowen@gmail.com>
2024-11-18 16:11:00 +0800

    更新：MDB

--- status.php
+++ status.php
@@ -14,8 +14,10 @@ if (!isset($_SESSION['username'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Status Page</title>
     <!-- 引入 MDB 的 CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/mdbootstrap@5.3.0/dist/css/mdb.min.css" rel="stylesheet">
-</head>
+    <link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+  rel="stylesheet"
+/></head>
 <body>
 
 <!-- Navbar -->
@@ -58,8 +60,10 @@ if (!isset($_SESSION['username'])) {
 </div>
 
 <!-- 引入 MDB 的 JS -->
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/mdbootstrap@5.3.0/dist/js/mdb.min.js"></script>
-
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/8.0.0/mdb.umd.min.js"
+></script>
 <script>
     // 動態時間與問候語
     function updateTime() {
